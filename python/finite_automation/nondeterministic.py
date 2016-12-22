7c375b210527de7d6556ef0ce260f34b7be54451
Prodesire <Prodesire@yeah.net>
2016-12-23 01:42:03 +0800

    implement NFA with free move

--- python/finite_automation/nondeterministic.py
+++ python/finite_automation/nondeterministic.py
@@ -15,6 +15,12 @@ class NFARulebook(object):
         return [rule.follow() for rule in self.rules
                 if rule.applies_to(state, character)]
 
+    def follow_free_moves(self, states):
+        more_states = self.next_state(states, None)
+        if more_states < states:
+            return states
+        return set(self.follow_free_moves(more_states | states))
+
 
 class NFA(object):
     def __init__(self, current_states, accept_states, rulebook):
@@ -23,10 +29,13 @@ class NFA(object):
         self.rulebook = rulebook
 
     def accepting(self):
-        return bool(self.current_states & self.accept_states)
+        return bool(self.get_current_states() & self.accept_states)
+
+    def get_current_states(self):
+        return self.rulebook.follow_free_moves(self.current_states)
 
     def read_character(self, character):
-        self.current_states = self.rulebook.next_state(self.current_states, character)
+        self.current_states = self.rulebook.next_state(self.get_current_states(), character)
 
     def read_string(self, string):
         for c in string:
@@ -40,24 +49,40 @@ class NFADesign(object):
         self.rulebook = rulebook
 
     def accepts(self, string):
-        nfa = NFA(self.start_state, self.accept_states, self.rulebook)
+        nfa = NFA({self.start_state}, self.accept_states, self.rulebook)
         nfa.read_string(string)
         return nfa.accepting()
 
 
-rulebook = NFARulebook([
-    FARule(1, 'a', 1), FARule(1, 'b', 1), FARule(1, 'b', 2),
-    FARule(2, 'a', 3), FARule(2, 'b', 3),
-    FARule(3, 'a', 4), FARule(3, 'b', 4)
-])
+# rulebook = NFARulebook([
+#     FARule(1, 'a', 1), FARule(1, 'b', 1), FARule(1, 'b', 2),
+#     FARule(2, 'a', 3), FARule(2, 'b', 3),
+#     FARule(3, 'a', 4), FARule(3, 'b', 4)
+# ])
 
 # # Test NFA
-# nfa = NFA({1}, {4}, rulebook)
+# nfa = NFA(1, {4}, rulebook)
 # nfa.read_string('bbbbb')
 # print nfa.accepting()
 #
 # # Test NFADesign
-# nfa_design = NFADesign({1}, {4}, rulebook)
+# nfa_design = NFADesign(1, {4}, rulebook)
 # print nfa_design.accepts('bab')
 # print nfa_design.accepts('bbbbb')
 # print nfa_design.accepts('bbabb')
+
+
+# Test NFA with free move
+rulebook = NFARulebook([
+    FARule(1, None, 2), FARule(1, None, 4),
+    FARule(2, 'a', 3),
+    FARule(3, 'a', 2),
+    FARule(4, 'a', 5),
+    FARule(5, 'a', 6),
+    FARule(6, 'a', 4),
+])
+nfa_design = NFADesign(1, {2, 4}, rulebook)
+print nfa_design.accepts('aa')
+print nfa_design.accepts('aaa')
+print nfa_design.accepts('aaaaa')
+print nfa_design.accepts('aaaaaa')
--- ruby/finite_automation/nondeterministic.rb
+++ ruby/finite_automation/nondeterministic.rb
@@ -11,6 +11,16 @@ class NFARulebook < Struct.new(:rules)
     rules_for(state, character).map(&:follow)
   end
 
+  def follow_free_moves(states)
+    more_states = next_state(states, nil)
+
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
+
   def rules_for(state, character)
     rules.select { |rule| rule.applies_to?(state, character) }
   end
@@ -20,6 +30,10 @@ end
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
   def accepting?
     (current_states & accept_states).any?
+  end
+
+  def current_states
+    rulebook.follow_free_moves(super)
   end
 
   def read_character(character)
@@ -45,19 +59,35 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
 end
 
 
+# rulebook = NFARulebook.new([
+#     FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+#     FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+#     FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+# ])
+#
+# # Test NFA
+# nfa = NFA.new(Set[1], [4], rulebook)
+# nfa.read_string('bbbbb')
+# puts nfa.accepting?
+#
+# # Test NFADesign
+# nfa_design = NFADesign.new(1, [4], rulebook)
+# puts nfa_design.accepts?('bab')
+# puts nfa_design.accepts?('bbbbb')
+# puts nfa_design.accepts?('bbabb')
+
+
+# Test NFA with free move
 rulebook = NFARulebook.new([
-    FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-    FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-    FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+    FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+    FARule.new(2, 'a', 3),
+    FARule.new(3, 'a', 2),
+    FARule.new(4, 'a', 5),
+    FARule.new(5, 'a', 6),
+    FARule.new(6, 'a', 4),
 ])
-
-# Test NFA
-nfa = NFA.new(Set[1], [4], rulebook)
-nfa.read_string('bbbbb')
-puts nfa.accepting?
-
-# Test NFADesign
-nfa_design = NFADesign.new(1, [4], rulebook)
-puts nfa_design.accepts?('bab')
-puts nfa_design.accepts?('bbbbb')
-puts nfa_design.accepts?('bbabb')
+nfa_design = NFADesign.new(1, [2, 4], rulebook)
+puts nfa_design.accepts?('aa')
+puts nfa_design.accepts?('aaa')
+puts nfa_design.accepts?('aaaaa')
+puts nfa_design.accepts?('aaaaaa')
